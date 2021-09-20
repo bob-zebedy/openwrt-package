5614339f1518deb1ec13bac74a487c8f473aeaee
Bob <zhang.zebedy@gmail.com>
2021-09-20 14:53:17 +0800

    luci-app-eqos: reformat

--- luci-app-eqos/luasrc/controller/eqos.lua
+++ luci-app-eqos/luasrc/controller/eqos.lua
@@ -1,12 +1,10 @@
 module("luci.controller.eqos", package.seeall)
 
 function index()
-	if not nixio.fs.access("/etc/config/eqos") then
-		return
-	end
-	
-	local page
-	
-	page = entry({"admin", "services", "eqos"}, cbi("eqos"), _("EQoS"), 1000)
-	page.dependent = true
+    if not nixio.fs.access("/etc/config/eqos") then return end
+
+    local page
+
+    page = entry({"admin", "services", "eqos"}, cbi("eqos"), _("EQoS"), 1000)
+    page.dependent = true
 end
--- luci-app-eqos/luasrc/model/cbi/eqos.lua
+++ luci-app-eqos/luasrc/model/cbi/eqos.lua
@@ -8,24 +8,27 @@ s.anonymous = true
 local e = s:option(Flag, "enabled", translate("Enable"))
 e.rmempty = false
 
-local dl = s:option(Value, "download", translate("Download speed (Mbit/s)"), translate("Total bandwidth"))
+local dl = s:option(Value, "download", translate("Download speed (Mbit/s)"),
+                    translate("Total bandwidth"))
 dl.datatype = "and(uinteger,min(1))"
 
-local ul = s:option(Value, "upload", translate("Upload speed (Mbit/s)"), translate("Total bandwidth"))
+local ul = s:option(Value, "upload", translate("Upload speed (Mbit/s)"),
+                    translate("Total bandwidth"))
 ul.datatype = "and(uinteger,min(1))"
 
-s = m:section(TypedSection, "device", translate("Speed limit based on IP address"))
+s = m:section(TypedSection, "device",
+              translate("Speed limit based on IP address"))
 s.template = "cbi/tblsection"
 s.anonymous = true
 s.addremove = true
-s.sortable  = true
+s.sortable = true
 
 local ip = s:option(Value, "ip", translate("IP address"))
 
 ipc.neighbors({family = 4, dev = "br-lan"}, function(n)
-	if n.mac and n.dest then
-		ip:value(n.dest:string(), "%s (%s)" %{ n.dest:string(), n.mac })
-	end
+    if n.mac and n.dest then
+        ip:value(n.dest:string(), "%s (%s)" % {n.dest:string(), n.mac})
+    end
 end)
 
 dl = s:option(Value, "download", translate("Download speed (Mbit/s)"))
