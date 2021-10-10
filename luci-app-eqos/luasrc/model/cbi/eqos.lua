80a32f82d33dae543cd8e86f162be57c0fa4bca2
Bob <zhang.zebedy@gmail.com>
2021-10-10 11:12:31 +0800

    update

--- luci-app-eqos/luasrc/model/cbi/eqos.lua
+++ luci-app-eqos/luasrc/model/cbi/eqos.lua
@@ -8,16 +8,13 @@ s.anonymous = true
 local e = s:option(Flag, "enabled", translate("Enable"))
 e.rmempty = false
 
-local dl = s:option(Value, "download", translate("Download speed (Mbit/s)"),
-                    translate("Total bandwidth"))
+local dl = s:option(Value, "download", translate("Download speed (Mbit/s)"), translate("Total bandwidth"))
 dl.datatype = "and(uinteger,min(1))"
 
-local ul = s:option(Value, "upload", translate("Upload speed (Mbit/s)"),
-                    translate("Total bandwidth"))
+local ul = s:option(Value, "upload", translate("Upload speed (Mbit/s)"), translate("Total bandwidth"))
 ul.datatype = "and(uinteger,min(1))"
 
-s = m:section(TypedSection, "device",
-              translate("Speed limit based on IP address"))
+s = m:section(TypedSection, "device", translate("Speed limit based on IP address"))
 s.template = "cbi/tblsection"
 s.anonymous = true
 s.addremove = true
