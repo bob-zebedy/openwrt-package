833abe909fb527e7206c967882a3b4f2cfd04dce
Bobby <zhang.deplives@hotmail.com>
2021-10-17 12:19:57 +0800

    add luci-app-aliddns

---
+++ luci-app-aliddns/luasrc/controller/aliddns.lua
@@ -0,0 +1,12 @@
+module("luci.controller.aliddns", package.seeall)
+
+function index()
+    if not nixio.fs.access("/etc/config/aliddns") then 
+        return 
+    end
+
+    local page
+    
+    page = entry({"admin", "services", "aliddns"}, cbi("aliddns"), _("AliDDNS"), 30)
+    page.dependent = true
+end
