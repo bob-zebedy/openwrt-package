0d9465b91bc37d079a52a493b417b8179e3a6b52
Bob <zhang.zebedy@gmail.com>
2021-10-17 12:16:05 +0800

    update

--- luci-app-eqos/luasrc/controller/eqos.lua
+++ luci-app-eqos/luasrc/controller/eqos.lua
@@ -1,10 +1,12 @@
 module("luci.controller.eqos", package.seeall)
 
 function index()
-    if not nixio.fs.access("/etc/config/eqos") then return end
+    if not nixio.fs.access("/etc/config/eqos") then 
+        return 
+    end
 
     local page
 
-    page = entry({"admin", "services", "eqos"}, cbi("eqos"), _("EQoS"), 30)
+    page = entry({"admin", "services", "eqos"}, cbi("eqos"), _("EQoS"), 40)
     page.dependent = true
 end
