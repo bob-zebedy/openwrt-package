ed78961e46bd4411d7219b69e22cde4cfc86a617
Bobby <zhang.deplives@hotmail.com>
2021-10-25 14:13:28 +0800

    更新翻译

--- luci-app-ipsec-server/luasrc/controller/ipsec-server.lua
+++ luci-app-ipsec-server/luasrc/controller/ipsec-server.lua
@@ -7,7 +7,7 @@ function index()
 	end
 	
 	entry({"admin", "vpn"}, firstchild(), "VPN", 45).dependent = false
-	entry({"admin", "vpn", "ipsec-server"}, cbi("ipsec-server/ipsec-server"), _("IPSec VPN Server"), 80).dependent = false
+	entry({"admin", "vpn", "ipsec-server"}, cbi("ipsec-server/ipsec-server"), _("IPSec VPN"), 80).dependent = false
 	entry({"admin", "vpn", "ipsec-server", "status"}, call("act_status")).leaf = true
 end
 
