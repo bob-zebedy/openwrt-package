fea09d2c5df2e1118f39c6c86a7e87c1f608ba94
Bob <zhang.zebedy@gmail.com>
2023-03-16 12:48:21 +0800

    移除 luci-app-bobby-wireguard

--- luci-app-bobby-wireguard/luasrc/controller/wireguard.lua
+++
@@ -1,9 +0,0 @@
-module("luci.controller.wireguard", package.seeall)
-
-function index()
-    entry({
-        "admin",
-        "status",
-        "wireguard"
-    }, template("wireguard"), _("WireGuard"), 92)
-end
